d222c4027df0d3fc08cf6c72860cf7ecf9407188
debpu06 <david.boland@protonmail.com>
2019-11-24 21:32:15 -0600

    Removed callback that was preventing functionality from running through

--- src/lambda/contactForm.js
+++ src/lambda/contactForm.js
@@ -3,10 +3,10 @@ const nodemailer = require('nodemailer');
 export function handler(event, context, callback) {
     const data = JSON.parse(event.body);
 
-    callback(null, {
-        statusCode: 200, 
-        body: JSON.stringify({ message: data })
-    });
+    // callback(null, {
+    //     statusCode: 200, 
+    //     body: JSON.stringify({ message: data })
+    // });
     var transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
